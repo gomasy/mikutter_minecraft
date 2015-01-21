235e1b8c938d1007c8bf78a5e53d56ba3fd48ac8
Gomasy <nyan@gomasy.jp>
2015-01-21 20:54:29 +0900

    アホでした

--- mikutter_minecraft.rb
+++ mikutter_minecraft.rb
@@ -4,20 +4,20 @@ PORT = 29653
 
 Plugin.create(:mikutter_minecraft) do
   filter_gui_postbox_post do |gui_postbox|
-    Plugin.call(:before_postbox_post, Plugin.create(:gtk).widgetof(gui_postbox).widget_post.buffer.text)
-    Plugin.create(:gtk).widgetof(gui_postbox).widget_post.buffer.text = ""
-    Plugin.filter_cancel!
-  end
-
-  on_before_postbox_post do |mes|
-    if mes =~ /^@mc\s(.+)$/
+    str = Plugin.create(:gtk).widgetof(gui_postbox).widget_post.buffer.text
+    if str =~ /^@mc\s(.+)$/
       begin
         s = TCPSocket.open("localhost", PORT)
         s.puts($1)
         s.close
       rescue Errno::ECONNREFUSED => e
         activity(:system, "通信に失敗しました。Minecraft が起動していることを確認してください。 - #{e.message}")
+      ensure
+        Plugin.call(:before_postbox_post, str)
+        Plugin.create(:gtk).widgetof(gui_postbox).widget_post.buffer.text = ""
+        Plugin.filter_cancel!
       end
     end
+    [gui_postbox]
   end
 end
