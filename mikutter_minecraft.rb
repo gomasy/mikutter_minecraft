63ca56d0067d05fa2ee884b1558ab013ffe8e7a2
Gomasy <nyan@gomasy.jp>
2015-01-23 15:31:48 +0900

    fix

--- mikutter_minecraft.rb
+++ mikutter_minecraft.rb
@@ -1,15 +1,16 @@
 require "socket"
+require "cgi"
 
-PORT = 29653
+S_PORT, R_PORT = 29653, 29654
 
 Plugin.create(:mikutter_minecraft) do
   filter_gui_postbox_post do |gui_postbox|
     str = Plugin.create(:gtk).widgetof(gui_postbox).widget_post.buffer.text
     if str =~ /^@mc\s(.+)$/
       begin
-        s = TCPSocket.open("localhost", PORT)
-        s.puts($1)
-        s.close
+        sock = TCPSocket.open("localhost", S_PORT)
+        sock.puts(CGI.escape($1))
+        sock.close
       rescue Errno::ECONNREFUSED => e
         activity(:system, "通信に失敗しました。Minecraft が起動していることを確認してください。 - #{e.message}")
       ensure
@@ -20,4 +21,21 @@ Plugin.create(:mikutter_minecraft) do
     end
     [gui_postbox]
   end
+
+  th = []
+  th << Thread.new do
+    srv = TCPServer.open(R_PORT)
+
+    loop do
+      sock = srv.accept
+
+      while buf = sock.gets
+        activity(:system, CGI.unescape(buf))
+      end
+
+      sock.close
+    end
+  end
+
+  th.join
 end
