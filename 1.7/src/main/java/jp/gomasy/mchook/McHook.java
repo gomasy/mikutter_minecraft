1864de232a76b60a72525d9b574d5d5984d9c806
Gomasy <nyan@gomasy.jp>
2015-02-26 02:31:50 +0900

    1.05(for 1.7): build using forge 1.7.10-10.13.2.1291

--- 1.7/src/main/java/jp/gomasy/mchook/McHook.java
+++ 1.7/src/main/java/jp/gomasy/mchook/McHook.java
@@ -12,7 +12,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 public class McHook
 {
     public static final String MODID = "jp.gomasy.mchook";
-    public static final String VERSION = "1.04";
+    public static final String VERSION = "1.05";
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
