eb0cb9c26e24caf7b9627bf4d638418275e6cd30
Gomasy <nyan@gomasy.jp>
2015-02-26 02:32:20 +0900

    1.05(for 1.8): build using forge 1.8-11.14.0.1299

--- 1.8/src/main/java/jp/gomasy/mchook/McHook.java
+++ 1.8/src/main/java/jp/gomasy/mchook/McHook.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class McHook
 {
     public static final String MODID = "jp.gomasy.mchook";
-    public static final String VERSION = "1.04";
+    public static final String VERSION = "1.05";
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
